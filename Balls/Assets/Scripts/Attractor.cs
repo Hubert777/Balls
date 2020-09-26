0140b377f22ffa4bbb6e1a088dc32c6264a4d8ad
hubert niewinski <Puzon2727@outlook.com>
2020-09-26 03:39:44 +0200

    Initial commit

---
+++ Balls/Assets/Scripts/Attractor.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+
+    //const float G = 667.4f;
+    const float G = 1f;
+
+    public static List<Attractor> Attractors;
+
+    public float gravityField;
+    
+    public Rigidbody rb;
+    
+
+    void FixedUpdate()
+    {
+        foreach (Attractor attractor in Attractors)
+        {
+            if (attractor.gameObject.GetComponent<Rigidbody>().mass >= 50)
+            {
+                attractor.gameObject.tag = "Boom";
+            }
+            if (attractor != this)
+                Attract(attractor);
+        }
+    }
+
+    void OnEnable()
+    {
+        if (Attractors == null)
+            Attractors = new List<Attractor>();
+
+        Attractors.Add(this);
+    }
+
+    void OnDisable()
+    {
+        Attractors.Remove(this);
+    }
+
+    void Attract(Attractor objToAttract)
+    {
+        Rigidbody rbToAttract = objToAttract.rb;
+
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        if (distance == 0f)
+            return;
+        else if (distance<gravityField)
+        {
+            float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+            Vector3 force = direction.normalized * forceMagnitude;
+
+            rbToAttract.AddForce(force);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (PlayerPrefs.GetInt("Permission")==1)
+        {
+            if (transform.position.x > collision.gameObject.transform.position.x)
+            {
+                float thisScale = transform.localScale.x;
+                float colliderScale = collision.transform.localScale.x;
+
+                if (thisScale < colliderScale)
+                {
+                    transform.position = collision.transform.position;
+                }
+
+                rb.mass += collision.rigidbody.mass;
+                gravityField += collision.rigidbody.mass;
+
+                this.gameObject.transform.localScale = new Vector3(thisScale + colliderScale, thisScale + colliderScale, thisScale + colliderScale);
+
+                rb.velocity = new Vector3(0, 0, 0);
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
---
+++ Balls/Assets/Scripts/BallController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+    
+    public GameObject ball;
+
+    int counterOfBalls;
+    List<GameObject> Balls = new List<GameObject>();
+
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.SetInt("Permission", 1);
+        InvokeRepeating("SpawnBall", 0f, 0.25f);
+    }
+
+    void FixedUpdate()
+    {
+        GameObject g = GameObject.FindGameObjectWithTag("Boom");
+        if (g != null && PlayerPrefs.GetInt("Permission") == 1)
+        {
+            
+            Transform tr = g.transform;
+            Destroy(g);
+            
+            
+            for(int i = 0; i < 50; i++)
+            {
+                GameObject oneOfFifty = Instantiate(ball, tr.position, Quaternion.identity);
+                oneOfFifty.GetComponent<Attractor>().enabled = false;
+                oneOfFifty.GetComponent<Collider>().enabled = false;
+                Balls.Add(oneOfFifty);
+                Vector3 vector = new Vector3(Random.Range(-300,300), Random.Range(-300, 300), Random.Range(-300, 300));
+                oneOfFifty.GetComponent<Rigidbody>().AddForce(vector);
+                //ZWALNIANIE
+            }
+            StartCoroutine(TurnOnCollisions(Balls));
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        CountBalls();
+	}
+
+    void SpawnBall()
+    {
+        Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 40f));
+        Instantiate(ball, screenPosition, Quaternion.identity);
+    }
+
+    void CountBalls()
+    {
+        counterOfBalls = GameObject.FindGameObjectsWithTag("Ball").Length;
+        Debug.Log(counterOfBalls);
+        if (counterOfBalls >= 250)
+        {
+            PlayerPrefs.SetInt("Permission", 0);
+            CancelInvoke();
+        }
+    }
+
+    IEnumerator TurnOnCollisions(List<GameObject> balls)
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        foreach(var ball in balls)
+        {
+            ball.GetComponent<Collider>().enabled = true;
+            ball.GetComponent<Attractor>().enabled = true;
+        
+        }
+        Balls.Clear();
+    }
+}
